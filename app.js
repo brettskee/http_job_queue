061da7abee5d0076a12883d5938e385589852983
boichee <bhlevenson@gmail.com>
2015-05-16 21:29:13 -0700

    Everything seems to be working.

--- app.js
+++ app.js
@@ -26,167 +26,189 @@ var jobs = kue.createQueue();
 
 // We'll also want to make it easier to access the redis client that Kue creates
 var rdclient = kue.Job.client;
+global.lastJobId = 0; // Cheap workaround for the moment. And it doesn't work on the very first post request for some reason.
 
 // Set up body parser for processing POSTs
 app.use(bodyParser.urlencoded({extended: true}));
 
 // Now set up a hash where we can store completed jobs
 // Structure will be { jobId: jobResult }
-var completedJobs = {};
+app.use(function(req, res, next) {
 
-/*
- * Using Kue:
- * So. it essentially appears that using Kue is fairly straightforward.
- * Basically, I can create a jobs cue by running jobs = kue.createQueue();
- * Then, that "jobs" variable will give me access to the ability to create new jobs (using jobs.create())
- * and to process those jobs in various ways using jobs.process, the event handler that is called when a new job is created.
+	// Setting up some additional variables I think I'll need as middleware
+	req.completedJobs = req.completedJobs || {};
 
- * jobs.process() takes
- * a string (that defines the type of job to actually process)
- * a callback function that takes two params:
- * 1) job - the job being processed
- * 2) a function done to be called when the work of the processing is done
- * - note: done can also be called as done(err) with some error value that defines that an error occurred.
-
- * This is interesting because at the time of job creation you can also set a few different event handlers. Namely:
- * job.on('complete', cb), job.on('failure', cb) and these behave the way you'd expect.
- * 
- * My thought is:
- * Can I use the cb in the "failure" event to pass through an error object in case a HTTP request fails?
- * So can I say job.on('failure', function(err) {}), and then somehow use that err inside the failure callback?
-
- * Also, to make this work, I'm going to have to be able to look up the status of a completed job using its ID.
- * This also essentially means that I'll have to be able to get the job ID at the time a job is created using the POST Endpoint
- * and send it back to the user in the response.
-*/
-
-
-// Create:
-// verb: the HTTP verb to use for the request
-// url: the URL for the API call
-// params: An object with key:value pairs (optional for get, required for post)
-
-var create = function(verb, url, params) {
-
-	// Do some error checking
-	if(verb === 'post' && !params)
-		return "A post request must include a params object.";
-
-	// So, we create a new job
-	var job = jobs.create(verb, { url: url, params: params });
-
-	// Now set some event handlers for success and failure
-	job.on('complete', function() {
-		console.log('JOB #%d IS COMPLETE', job.id);
-	}).on('failed', function(err) {
-		console.log('JOB #%d FAILED', job.id);
-	})
-	// .on('result', function(res) {
-	// 	// res should now contain the result of the HTTP request
-	// 	console.log("Results of a job were returned:", res);
-	// });
-
-	job.save(); // saves the new job to the queue
-
-	// Now return the job id so we can send it back to the user
-	// NOTE: Might need to use (and then parse) workerId instead as the job ids seem to reset on each launch of the server.
-	return job.id;
-}
-
-/*
-Sample Job Object:
-{
-	id : "3",
-	type : "get",
-	data :  { 
-		url : "http : //www.google.com",
-		params : { 
-			first : "first",
-			second : "third"
-		}
-	},
-	priority : 0,
-	progress : 0,
-	state : "active",
-	created_at : "1431591922074",
-	promote_at : "1431591922074",
-	updated_at : "1431591922075",
-	started_at : 1431591922077,
-	workerId : "kue : Sacagawea.local : 63454 : get : 1",
-	attempts : {
-		made : 0,
-		remaining : 1,
-		max : 1
-	}
-}
-
-*/
-
-// Set the event handler for HTTP request jobs using GET
-jobs.process('get', function(job, done) {
-	var r = request.get(job.data.url, job.data.params || null, function(err, resp, body) {
-		// Now we'll decide what to do based on the results of the request
-		if(err || resp.statusCode !== 200) {
-			// All is not good in the hood, so end processing and throw the error returned by request
-			return done(err);
-		};
-
-		// Because kue doesn't support passing results back natively, I'm creating a new event for my own use
-		events.emit(job.id, "result", { responseBody: body });
-
-		// Now attach the results to the job itself in Redis
-		rdclient.hmset(['q:job:'+job.id, 'respBody', body], function(redisError, redisResult) {
-			if(redisError) return done(redisError);
-		})
-
-		// Not sure if I was supposed to implement persistence now that I think about it, so I'm just
-		// going to use a simple hash in JS for now to store the results of the HTTP requests
-
-		// For some reason we are not making it all the way to here... hmmm...
-		console.log("Debug here");
-		completedJobs[job.id.toString()] = body;
-		console.log(completedJobs);
-
-		done(); // End processing now that we've attached everything (may want to remove this to give callbacks time to happen)
-	});
-
+	next();
 });
 
-// Now let's get to routes
+
+// Now put my custom Kue functions into middleware
+app.use(function(req, res, next) {
+
+	// Helper functions
+	req.helpers = {};
+
+	// Helpers: encode ascii text in base64
+	req.helpers.base64encode = function base64encode(str) {
+		return new Buffer(str).toString('base64');
+	}
+
+	// Helpers: decode base64 text into ascii
+	req.helpers.base64decode = function base64decode(b64) {
+		return new Buffer(b64, 'base64').toString('ascii');
+	}
+
+	// Creating some functions to avoid some of the right drift in jobs.process
+	req.makeGetRequest = function(job, done) {
+		var r = request.get(job.data.url, function(err, resp, body) {
+			// Now we'll decide what to do based on the results of the request
+			if(err || resp.statusCode !== 200) {
+				// All is not good in the hood, so end processing and throw the error returned by request
+				return done(err);
+			};
+
+			// The request results should be back
+			// Store the response in a local hash so that we can access the results synchronously.
+			req.completedJobs[job.id] = req.helpers.base64encode(body); // base64encode the results to avoid problems with newlines etc.
+
+			// Now attach the results to the job itself in Redis
+			rdclient.hmset(['q:job:'+job.id, 'respBody', body, 'respContentType', resp.headers['content-type'] ], function(redisError, redisResult) {
+				if(redisError) 
+					return done(redisError); // end if an error occurred.
+
+				// End processing now that we've stored the data in Redis
+				return done(); 
+			});
+		});
+	}
+
+	// Creating some functions to avoid some of the right drift in jobs.process
+	req.makePostRequest = function(job, done) {
+		var r = request.get(job.data.url, job.data.params, function(err, resp, body) {
+			// Now we'll decide what to do based on the results of the request
+			if(err || resp.statusCode !== 200) {
+				// All is not good in the hood, so end processing and throw the error returned by request
+				return done(err);
+			};
+
+			// The request results should be back
+			// Store the response in a local hash so that we can access the results synchronously.
+			req.completedJobs[job.id] = req.helpers.base64encode(body); // base64encode the results to avoid problems with newlines etc.
+
+			// Now attach the results to the job itself in Redis
+			rdclient.hmset(['q:job:'+job.id, 'respBody', body, 'respContentType', resp.headers['content-type'] ], function(redisError, redisResult) {
+				if(redisError) 
+					return done(redisError); // end if an error occurred.
+
+				// End processing now that we've stored the data in Redis
+				return done(); 
+			});
+		});
+	}
+
+	// Set the event handlers for HTTP request jobs using GET and POST
+	jobs.process('get', req.makeGetRequest);
+	jobs.process('post', req.makePostRequest);
+
+
+	// Create function:
+	// verb: the HTTP verb to use for the request
+	// url: the URL for the API call
+	// params: An object with key:value pairs (optional for get, required for post)
+	// cb: A callback to execute the response to the client
+
+	req.create = function(verb, url, params, cb) {
+
+		// Do some error checking to make sure all post requests include a params object
+		if(verb === 'post' && !params)
+			return "A post request must include a params object.";
+
+		// So, we create a new job
+		var job = jobs.create(verb, { url: url, params: params });
+
+		// Now set some event handlers for success and failure
+		job.on('complete', function() {
+			console.log('JOB #%d IS COMPLETE', job.id);
+		}).on('failed', function(err) {
+			console.log('JOB #%d FAILED', job.id);
+		});
+
+		// Essentially, I'm using an immediately invoked function, that takes a callback as a parameter
+		// as a way to ensure I'll be setting req.lastJob at a time when that information is available.
+
+
+		job.save(function(err) {
+			console.log("job.save() started.");
+			console.log("this callback should only be triggered once the save has occurred and jobId #%d is available.", job.id);
+
+			// Check if an error occurred on save
+			if(err) 
+				console.log("problem saving job with id", job.id);
+
+			cb(job);
+
+			// process.nextTick(function() { cb(job); });
+
+			console.log("job.save() finished.");
+
+		}); // saves the new job to the queue
+
+	};
+
+	next();
+
+});
 
 // GET job by id
 app.get('/jobs/:id', function(req, res) {
 	var jobId = req.params.id;
 
-	console.log(completedJobs);
-
 	// Look up the job in the redis db
-	rdclient.hget(['q:job:'+jobId, 'respBody'], function(redisError, redisResult) {
+	rdclient.hmget(['q:job:'+jobId, 'respBody', 'respContentType'], function(redisError, response) {
 		if(redisError) {
-			res.set('Content-Type', 'text/plain');
-			res.send("Job " + jobId + " not yet complete.");
+			res.set('Content-Type', 'text/html');
+			res.send("<h2>Job " + jobId + " not yet complete.</h2><p><a html=\"/jobs/"+jobId+"\">Click to Repeat Your Request</a></p>");
 		} else {
+			// Grab data from redis
+			var contentType = response[1];
+			var content = response[0];
+
 			// Send back the result
-			if(redisResult.indexOf("<!doctype") === -1) {
-				res.set('Content-Type', 'application/json');
-			} else {
-				res.set('Content-Type', 'text/html');
-			}
-			res.send(redisResult);
+			res.set('Content-Type', contentType);
+			res.send(content);
 		}
 	});
 });
 
+// Here's a post version of the add to queue endpoint
 app.post('/jobs', function(req, res) {
+
+	// Initialize some vars based on parsed body
 	var body = req.body;
 	body.params = body.params || null;
+	var verb = body.method || "get";
 
-	var jobId = create('get', body.url, body.params);
+	// Create is not returning a jobId properly. Not really surprising now that I looked again.
+	// Need to think about how to get this jobId into the response without doing something awful in the global scope like I currently am.
+	req.create(verb, body.url, body.params, function(lastJob) {
+		console.log("job.save() callback has started running.");
 
-	res.set('Content-Type', 'text/plain');
-	res.send("Job #" + jobId + " has been queued. Use POST /jobs/:id to check its status (and retrieve results once available.)");
+		// If user includes a query param f with value text, send a plain text response back to the client
+		if(req.query.f === "text") {
+			res.set('Content-Type', 'text/plain');
+			var textResponse = "Job #" + lastJob.id + " has been queued.\n";
+			textResponse += "Use GET /jobs/"+lastJob.id+" to check the status of your job.";
+			res.send(textResponse);
+		} else {
+			res.set('Content-Type', 'text/html');
+			var htmlResponse = "<h2>Job #" + lastJob.id + " has been queued.</h2>";
+			htmlResponse += "<p>Use GET /jobs/"+lastJob.id+" to check its status (and retrieve results once available.)</p>";
+			htmlResponse += "<p>You can also <a href=\"/jobs/"+lastJob.id+"\">click here</a> to check and see if the results are available.";
+			res.send(htmlResponse);
+		}
 
-
+		console.log("job.save() callback has finished running.");
+	});
 });
 
 app.listen(3000, function() {
